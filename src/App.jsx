6313e3c418e5152ae05937c46815598a93fba04a
Daewoong <kodw.kor@gmail.com>
2024-08-24 11:18:11 +0900

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import { useState, useEffect } from 'react'
 import './App.css'
+import { Box, CircularProgress, Container, Typography } from '@mui/material';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, setState] = useState();
   useEffect(() => {
     fetch("https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=155b2ed5bf7acdf91c902ae7ee3428ad&units=metric")
       .then((data) => data.json())
-      .then((data) => {console.log(data)});
+      .then((data) => {
+        setState(data);
+      });
   }, []);
 
   return (
-    <div>
-      <h1>Weather App</h1>
-    </div>
+    <Container maxWidth="xs">
+      <Typography variant='h3'>Weather App</Typography>
+      {state ? (
+        <Box marginTop="20px">
+          <Typography variant='h4'>{state.main.temp}°</Typography>
+        </Box>
+      ) : (
+        <CircularProgress />
+      )}
+    </Container>
   )
 }
 
